52fe9249c9c9eb901296bfdf4463f3f07d85878f
gymad <gyula.madarasz@salesagility.com>
2018-06-19 20:28:33 +0100

    removing a commented code

--- tests/unit/modules/Contacts/ContactTest.php
+++ tests/unit/modules/Contacts/ContactTest.php
@@ -348,7 +348,7 @@ class ContactTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
     	$contact = new Contact();

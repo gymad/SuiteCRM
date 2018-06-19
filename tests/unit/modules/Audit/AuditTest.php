d9acee8b2cbd46b571887d2e14c8b3d686678e18
gymad <gyula.madarasz@salesagility.com>
2018-06-19 20:28:03 +0100

    removing a commented code

--- tests/unit/modules/Audit/AuditTest.php
+++ tests/unit/modules/Audit/AuditTest.php
@@ -145,7 +145,7 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $audit = new Audit();

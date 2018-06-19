46d14a623df67d306eac377b27ce6988c12a51e7
gymad <gyula.madarasz@salesagility.com>
2018-06-19 20:25:02 +0100

    removing a commented code

--- tests/unit/include/MVC/Controller/SugarControllerTest.php
+++ tests/unit/include/MVC/Controller/SugarControllerTest.php
@@ -70,7 +70,7 @@ class SugarControllerTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
 	// test
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $SugarController = new SugarController();

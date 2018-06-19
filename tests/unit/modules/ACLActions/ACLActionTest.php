487620612a735d01e8594214c9a6b57d37bc43d5
gymad <gyula.madarasz@salesagility.com>
2018-06-19 20:27:17 +0100

    removing a commented code

--- tests/unit/modules/ACLActions/ACLActionTest.php
+++ tests/unit/modules/ACLActions/ACLActionTest.php
@@ -51,7 +51,7 @@ $_POST['foo'] = 'bar123ase';
         $state->pushTable('acl_actions');
         $state->pushTable('aod_index');
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
 
         //take count of actions initially and then after method execution and test if action count increases
         $action_count = count(ACLAction::getDefaultActions());
